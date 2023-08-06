339d2ade63698f2e525fdb1c9c6bf8af97808348
lovekeshpal <lovekeshfgr@gmail.com>
2023-08-06 12:55:12 +0530

    HEROES Object Display and Show/Hide Editing Component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from 'src/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
-export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+
+  ngOnInit() {}
+
+  selectedHero: Hero = { id: 0, name: 'none' };
+  showHero: boolean = false;
+
+  onSelect(hero: Hero, id: number): void {
+    if (id === this.selectedHero?.id) {
+      this.selectedHero = { id: 0, name: 'none' };
+      this.showHero = false;
+    } else {
+      this.selectedHero = hero;
+      this.showHero = true;
+    }
+  }
 }
