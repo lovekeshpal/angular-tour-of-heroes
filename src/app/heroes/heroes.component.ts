1bacfb4cf22bf99a66f43d09b2f4269f0e3d2c87
lovekeshpal <lovekeshfgr@gmail.com>
2023-08-09 16:18:59 +0530

    fetched data with services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from 'src/mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,12 +8,22 @@ import { HEROES } from 'src/mock-heroes';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[] = [];
 
-  ngOnInit() {}
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit() {
+    this.getHeroes();
+  }
 
   selectedHero?: Hero;
   showHero: boolean = false;
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => {
+      this.heroes = heroes;
+    });
+  }
 
   onSelect(hero: Hero, id: number): void {
     this.selectedHero = hero;
